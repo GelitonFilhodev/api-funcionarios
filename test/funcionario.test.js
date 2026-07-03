1e353b29d6f4d4bf83310d54b52888e2770629d8
Geliton <gelitonfilhodev@gmail.com>
2026-07-03 18:06:56 -0300

    Primeiro Commit

---
+++ test/funcionario.test.js
@@ -0,0 +1,19 @@
+const request - require("supertest")
+
+const app = require("../app")
+
+describe("API de Funcionarios",()=>{
+    it("Deve cadastrar um funcionario", async ()=>{
+        const resposta = await request(app)
+        .post("/funcionarios")
+        .send({
+            nome:"Maria",
+            cargo:"Analista"
+        })
+        expect(resposta.statusCode.toBe(201))
+
+        expect(resposta.body.nome).toBe("Maria")
+
+        expect(resposta.body.cargo).toBe("Analista")
+    })
+})
