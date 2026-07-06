9f0a6dc8baf84f79932ab9c658470aae9b6f05a2
Geliton <gelitonfilhodev@gmail.com>
2026-07-06 18:35:21 -0300

    Adicionando Banco de Dados

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const routes = require("./routes/funcionarioRouter")
+const sequelize = require("./config/database")
+
+const app = express()
+
+app.use(express.json())
+app.use(routes)
+
+sequelize.sync()
+
+module.exports = app
